3a8304b2e572f301775af7354d43bc7d581d0b84
Bruno Antônio Joanucci <bruno.joanucci@aluno.imepac.edu.br>
2024-04-03 19:40:36 -0300

    adicionado novos atributos

--- src/main/kotlin/com/finan/orcamento/model/OrcamentoModel.java
+++ src/main/kotlin/com/finan/orcamento/model/OrcamentoModel.java
@@ -2,17 +2,14 @@ package com.finan.orcamento.model;
 
 import com.finan.orcamento.model.enums.IcmsEstados;
 import jakarta.persistence.*;
-import lombok.AllArgsConstructor;
-import lombok.Data;
-import lombok.NoArgsConstructor;
+
 import org.jetbrains.annotations.NotNull;
 
 import java.io.Serializable;
 import java.math.BigDecimal;
+import java.math.BigInteger;
 
-@Data
-@NoArgsConstructor
-@AllArgsConstructor
+
 @Entity
 @Table(name="orcamento")
 public class OrcamentoModel implements Serializable {
@@ -33,6 +30,69 @@ public class OrcamentoModel implements Serializable {
     @ManyToOne
     @JoinColumn(name="usuario_id", referencedColumnName = "id")
     private UsuarioModel usuario;
+
+    @Column(name="qtd_itens")
+    private BigInteger qtdItens;
+
+    @Column(name="desconto_orcamento")
+    private BigDecimal descontoOrcamento;
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public IcmsEstados getIcmsEstados() {
+        return icmsEstados;
+    }
+
+    public void setIcmsEstados(IcmsEstados icmsEstados) {
+        this.icmsEstados = icmsEstados;
+    }
+
+    @NotNull
+    public BigDecimal getValorOrcamento() {
+        return valorOrcamento;
+    }
+
+    public void setValorOrcamento(@NotNull BigDecimal valorOrcamento) {
+        this.valorOrcamento = valorOrcamento;
+    }
+
+    public BigDecimal getValorICMS() {
+        return valorICMS;
+    }
+
+    public void setValorICMS(BigDecimal valorICMS) {
+        this.valorICMS = valorICMS;
+    }
+
+    public UsuarioModel getUsuario() {
+        return usuario;
+    }
+
+    public void setUsuario(UsuarioModel usuario) {
+        this.usuario = usuario;
+    }
+
+    public BigInteger getQtdItens() {
+        return qtdItens;
+    }
+
+    public void setQtdItens(BigInteger qtdItens) {
+        this.qtdItens = qtdItens;
+    }
+
+    public BigDecimal getDescontoOrcamento() {
+        return descontoOrcamento;
+    }
+
+    public void setDescontoOrcamento(BigDecimal descontoOrcamento) {
+        this.descontoOrcamento = descontoOrcamento;
+    }
 
     public void calcularIcms() {
         this.valorICMS = this.icmsEstados.getStrategy().calcular(this.valorOrcamento);
