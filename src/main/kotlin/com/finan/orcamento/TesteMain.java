e3d6c3ffadcb89d434e4f2296452ef67c5167862
Bruno Antônio Joanucci <brunonintendobr@gmail.com>
2024-04-14 17:38:32 -0300

    Classe Main pronta para realizar as operações

--- src/main/kotlin/com/finan/orcamento/TesteMain.java
+++ src/main/kotlin/com/finan/orcamento/TesteMain.java
@@ -1,14 +1,23 @@
 package com.finan.orcamento;
 
 import java.math.BigDecimal;
+
+import com.finan.orcamento.controller.OrcamentoController;
 import com.finan.orcamento.model.OrcamentoModel;
 import com.finan.orcamento.model.proxy.OrcamentoProxy;
 import com.finan.orcamento.service.OrcamentoService;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.ApplicationContext;
 
+@SpringBootApplication
 public class TesteMain {
     public static void main(String[] args) {
-        OrcamentoService orcamentoService = new OrcamentoService();
-        OrcamentoModel orcamentoModel = orcamentoService.buscaId(1L);
+
+        ApplicationContext context = SpringApplication.run(TesteMain.class, args);
+        OrcamentoController orcamentoController = context.getBean(OrcamentoController.class);
+
+        OrcamentoModel orcamentoModel = orcamentoController.buscaPorId(3L).getBody();
         System.out.println("valor do orcamento ="+ orcamentoModel.getValorOrcamento());
         System.out.println("tempo para ver o desconto="+ orcamentoModel.getDescontoOrcamento());
 
